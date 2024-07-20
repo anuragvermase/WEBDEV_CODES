d4458e2a35dd98969b32af1e72cf366e18721683
Anurag Verma <anuragver689@gmail.com>
2024-07-20 21:49:51 +0530

    add

--- crud-sigma-english/controllers/userController.js
+++ crud-sigma-english/controllers/userController.js
@@ -1,3 +1,4 @@
+const { use } = require('../app.js')
 const User = require('../models/users.schema.js')
 
 exports.home = (req,res)=>{
@@ -37,6 +38,52 @@ exports.createUser = async(req, res)=>{
     }
 } 
 
-exports.getUsers = (req,res)=>{}
-exports.editUsers = (req,res)=>{}
-exports.deleteUser = (req,res)=>{}
+exports.getUsers = async(req,res)=>{
+    try {
+        const users = await User.find({})
+        res.status(200).json({
+            success : true,
+            users
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
+exports.editUsers = async(req,res)=>{
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            success : true,
+            message : "User updated successfully"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
+exports.deleteUser = async(req,res)=>{
+    try {
+        const userId = req.params.id
+        const user = await User.findByIdAndDelete(userId)
+        res.status(200).json({
+            success : true,
+            message : "user deleted successfully"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
--- crud-sigma-english/routes/userRoutes.js
+++ crud-sigma-english/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const {home,createUser} = require('../controllers/userController.js')
+const {home,createUser, getUsers, deleteUser, editUsers} = require('../controllers/userController.js')
 
 const router = express.Router();
 
 router.get('/', home)
 router.post('/createuser',createUser)
+router.get('/getuser',getUsers)
+router.delete('/deleteuser/:id', deleteUser)
+router.put('/updateuser/:id',editUsers)
 
 module.exports = router
